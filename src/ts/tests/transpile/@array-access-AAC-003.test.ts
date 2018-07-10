219b2075fd9a99ec4bcbe8b6a2772dcb88a5f21b
Wong Jia Hau <hou32hou@gmail.com>
2018-07-10 12:27:23 +0800

    Added AAC-003

---
+++ src/ts/tests/transpile/@array-access-AAC-003.test.ts
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+import { pine2js } from "../../pine2js";
+
+describe("@array-access-AAC-003", () => {
+    it("type inference on array element", () => {
+        const input =
+`
+def main:
+    let x = [1,2][0] + [2,3][0]
+`;
+        const expectedOutput =
+`
+function _main(){
+const $x = ((new ArrayOfNumber([(1),(2),]))[(0)]._$plus_Number((new ArrayOfNumber([(2),(3),]))[(0)]));
+}
+`.trim();
+
+        const result = pine2js(input).trim();
+        // console.log(expectedOutput);
+        // console.log(result);
+        expect(result).to.eq(expectedOutput);
+    });
+
+});
