e670ab03452f5bdee530ca3a637fdb6bdc75e61f
Wong Jia Hau <hou32hou@gmail.com>
2018-07-12 15:01:55 +0800

    Added test for insertChild

---
+++ src/ts/tests/typeTree/insertChild.test.ts
@@ -0,0 +1,102 @@
+import {expect} from "chai";
+import {prettyPrint} from "../../pine2js";
+import {initTypeTree, insertChild, newArrayType, newSimpleType, newTypeTree} from "../../typeTree";
+
+describe("insertChild", () => {
+    it("case 1", () => {
+        const objectType = newSimpleType("Object");
+        const arrayType = newArrayType(objectType);
+        const initTree = newTypeTree(objectType);
+        const newTree = insertChild(arrayType,
+        /*as child of*/
+        objectType,
+        /*in*/
+        initTree);
+        const expected = {
+            current: {
+                kind: "SimpleType",
+                name: {
+                    repr: "Object",
+                    location: null
+                },
+                nullable: false
+            },
+            children: [
+                {
+                    current: {
+                        kind: "ArrayType",
+                        arrayOf: {
+                            kind: "SimpleType",
+                            name: {
+                                repr: "Object",
+                                location: null
+                            },
+                            nullable: false
+                        },
+                        nullable: false
+                    },
+                    children: []
+                }
+            ]
+        };
+        expect(newTree)
+            .to
+            .deep
+            .eq(expected);
+    });
+
+    it("case 2", () => {
+        const objectType = newSimpleType("Object");
+        const arrayType = newArrayType(objectType);
+        const numberType = newSimpleType("Number");
+        const numberArrayType = newArrayType(numberType);
+        let tree = newTypeTree(objectType);
+        tree = insertChild(arrayType, objectType, tree);
+        tree = insertChild(numberArrayType, arrayType, tree);
+        const expected = {
+            current: {
+                kind: "SimpleType",
+                name: {
+                    repr: "Object",
+                    location: null
+                },
+                nullable: false
+            },
+            children: [
+                {
+                    current: {
+                        kind: "ArrayType",
+                        arrayOf: {
+                            kind: "SimpleType",
+                            name: {
+                                repr: "Object",
+                                location: null
+                            },
+                            nullable: false
+                        },
+                        nullable: false
+                    },
+                    children: [
+                        {
+                            current: {
+                                kind: "ArrayType",
+                                arrayOf: {
+                                    kind: "SimpleType",
+                                    name: {
+                                        repr: "Number",
+                                        location: null
+                                    },
+                                    nullable: false
+                                },
+                                nullable: false
+                            },
+                            children: []
+                        }
+                    ]
+                }
+            ]
+        };
+        expect(tree).to.deep.eq(expected);
+    });
+
+});
---
+++ src/ts/typeTree.ts
@@ -0,0 +1,55 @@
+import {ArrayType, SimpleType, TypeExpression} from "./ast";
+import { typeEquals } from "./fillUpTypeInformation";
+
+export interface TypeTree {
+    current: TypeExpression;
+    children: TypeTree[];
+}
+
+export function newTypeTree(type: TypeExpression): TypeTree {
+    return {
+        current: type,
+        children: []
+    };
+}
+
+export function insertChild(child: TypeExpression, parent: TypeExpression, tree: TypeTree): TypeTree {
+    if (typeEquals(parent, tree.current)) {
+        tree.children.push(newTypeTree(child));
+    } else {
+        for (let i = 0; i < tree.children.length; i++) {
+            tree.children[i] = insertChild(child, parent, tree.children[i]);
+        }
+    }
+    return tree;
+}
+
+export function initTypeTree(): TypeTree {
+    const objectType = newSimpleType("Object");
+    const arrayType = newArrayType(objectType);
+    const numberType = newSimpleType("Number");
+    const numberArrayType = newArrayType(numberType);
+    let tree = newTypeTree(objectType);
+    tree = insertChild(arrayType, objectType, tree);
+    tree = insertChild(numberArrayType, arrayType, tree);
+    return tree;
+}
+
+export function newSimpleType(name: string): SimpleType {
+    return {
+        kind: "SimpleType",
+        name: {
+            repr: name,
+            location: null
+        },
+        nullable: false
+    };
+}
+
+export function newArrayType(arrayOfWhat: TypeExpression): ArrayType {
+    return {
+        kind: "ArrayType" ,
+        arrayOf: arrayOfWhat,
+        nullable: false
+    };
+}
