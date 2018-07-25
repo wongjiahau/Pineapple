59210061b4af793a47bed5a2c8dc78de0d11cf1a
Wong Jia Hau <hou32hou@gmail.com>
2018-07-25 11:41:01 +0800

    Pass test FCMO-005

--- src/ts/getIntermediateForm.ts
+++ src/ts/getIntermediateForm.ts
@@ -25,7 +25,7 @@ export function getIntermediateForm(
         prevIntermediate.syntaxTrees = prevIntermediate.syntaxTrees.concat(newAst);
         return prevIntermediate;
     } catch (e) {
-        // console.log(e);
+        console.log(e);
         const error = (e as PineError);
         error.errorMessage =
             generateErrorMessage(
---
+++ src/ts/tests/transpile/func-call/monofunc/FCMO-005.test.ts
@@ -0,0 +1,31 @@
+import { pine2js } from "../../../../pine2js";
+import { assertEquals } from "../../../testUtil";
+
+describe("FCMO-005", () => {
+    it("generic monofunc", () => {
+        const input =
+`
+def (this T).show -> T
+    pass
+
+def .main
+    "Hello".show
+`;
+        const expectedOutput =
+`
+function _show_Generic$T($this){
+$$pass$$();
+}
+
+function _show_String($this){
+$$pass$$();
+}
+
+function _main_(){
+_show_String("Hello");
+}
+`;
+        assertEquals(pine2js(input).trim(), expectedOutput.trim());
+    });
+
+});
