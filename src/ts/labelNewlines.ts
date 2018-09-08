8d1b0acd572b71c6275609114b9fcd9a7cc9e6f8
Wong Jia Hau <hou32hou@gmail.com>
2018-09-08 16:58:43 +0800

    Fix a bug regarding splitting newlines on different platforms

--- src/ts/labelNewlines.ts
+++ src/ts/labelNewlines.ts
@@ -1,3 +1,5 @@
 export function labelNewlines(input: string): string {
-    return input.replace(/\n/g, "@NEWLINE\n");
+    return input.replace(/\r?\n/g, "@NEWLINE\n");   
 }
+
+// Refer: https://stackoverflow.com/questions/21895233/how-in-node-to-split-string-by-newline-n
