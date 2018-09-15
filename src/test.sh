b15753a2548d6a365332bb22c54734d193e804f3
Wong Jia Hau <hou32hou@gmail.com>
2018-09-15 13:04:07 +0800

    Updated test script

--- src/test.sh
+++ src/test.sh
@@ -2,7 +2,7 @@
 
 # This 0 option is needed for Travis CI
 if [ "$1" -eq 0 ]; then
-    ./node_modules/jest/bin/jest.js
+    ./node_modules/jest/bin/jest.js --coverage
 else
     ./node_modules/jest/bin/jest.js --watch
 fi
