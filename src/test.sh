e3c5b8825359cf5878a660f2bc1a7abb08ce3fd0
Wong Jia Hau <hou32hou@gmail.com>
2018-09-18 13:33:13 +0800

    Updated test script

--- src/test.sh
+++ src/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# This r option is needed for Travis CI
+# This 0 option is needed for Travis CI
 if [ "$1" = "0" ]; then
-    ./node_modules/jest/bin/jest.js 
+    ./node_modules/jest/bin/jest.js --ci
 else
     ./node_modules/jest/bin/jest.js --watch
 fi
