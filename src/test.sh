0a9004ab6216d629d4033efc857aac1650dfb076
Wong Jia Hau <hou32hou@gmail.com>
2018-09-18 13:58:05 +0800

    Updated test script

--- src/test.sh
+++ src/test.sh
@@ -2,7 +2,9 @@
 
 # This 0 option is needed for Travis CI
 if [ "$1" = "0" ]; then
-    ./node_modules/jest/bin/jest.js --ci
+    # Run test on compiled Javascripts, because using transformer is memory-expensive 
+    ./node_modules/jest/bin/jest.js './dist/*' --ci
 else
-    ./node_modules/jest/bin/jest.js --watch
+    # Run test on Typescript test files using transformer ts-jest
+    ./node_modules/jest/bin/jest.js './ts/*' --watch 
 fi
